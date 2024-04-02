ba973ab8a36c61aaebf07d32e807c5393b61b233
Usman Rajput <bcawala3@gmail.com>
2024-04-02 15:36:06 +0530

    Array

--- Array/basic.js
+++ Array/basic.js
@@ -1,21 +1,21 @@
 // // basic array
 
-// const friends = ["sahil", "aniket", "nitin"];
+const friends = ["sahil", "aniket", "nitin"];
 
-// // another method to declare arrr
+// another method to declare arrr
 
 // const arr = new Array("friend1", "friend2", "friend3");
 
-// // console.log(friends, arr);
+// console.log(friends, arr);
 
-// const Calcage =function(birthyear){
-//     return 2023 -birthyear
-// }
-// const years =[2002, 2003, 2001, 2005, 2010];
+const Calcage =function(birthyear){
+    return 2023 -birthyear
+}
+const years =[2002, 2003, 2001, 2005, 2010];
 
-// const ages = [Calcage(years[0]), Calcage(years[1]), Calcage(years[years.length-1])]
+const ages = [Calcage(years[0]), Calcage(years[1]), Calcage(years[years.length-1])]
 
-// console.log(ages)
+console.log(ages)
 const arr = ["aniket ", "sahil", "nitien"];
 
 const newArr = new Array("sahil", "shivam", "aniket", "anshul")
